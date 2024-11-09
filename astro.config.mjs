bd404f5a09dd6611591ff9649cefa23843e270b5
louis <louisgituhi@gmail.com>
2024-11-09 19:33:22 +0300

    geist font

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,14 @@ import tailwind from '@astrojs/tailwind';
 
 import db from '@astrojs/db';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db()]
+  integrations: [
+    tailwind({
+    applyBaseStyles: false
+  }), 
+  db(), 
+  react()]
 });
