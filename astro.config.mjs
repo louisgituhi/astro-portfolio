b906a1d0502f7bac5d7af838e2fb98b58ac45cb6
louis <louisgituhi@gmail.com>
2025-07-19 21:26:09 +0300

    Update project files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
@@ -6,6 +6,9 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: passthroughImageService()
+  },
   integrations: [
     tailwind({
     applyBaseStyles: false
